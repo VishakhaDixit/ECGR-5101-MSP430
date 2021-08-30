0ad42d4637a40c348dd5571d2e293072e4627184
VishakhaDixit <vdixit2@uncc.edu>
2021-08-30 14:27:34 -0400

    Modified gpio_set func

--- Project-1/main.c
+++ Project-1/main.c
@@ -7,16 +7,38 @@
 
 int main(void)
 {
-    bool switch_status;
 
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 
 	gpio_init(PORT_2, PIN_1, OUTPUT);   //Using P1.6 for led
 	gpio_init(PORT_1, PIN_3, INPUT);    //Using P1.3 for switch
-	
+
 	while(1)
 	{
 	    switch_status = gpio_get(PORT_1, PIN_3);
 	    gpio_set(PORT_2, PIN_1, switch_status);
 	}
+
+//	gpio_init(PORT_1, PIN_6, OUTPUT);   //Using P1.6 for led
+//
+//    while(1)
+//    {
+//        unsigned long i;
+//        gpio_set(PORT_1, PIN_6, true);
+//
+//        i = 50000;
+//        do
+//        {
+//            i--;
+//        } while( i != 0);
+//
+//        gpio_set(PORT_1, PIN_6, false);
+//
+//        i = 50000;
+//        do
+//        {
+//            i--;
+//        } while( i != 0);
+//    }
+	
 }
--- source/gpio.c
+++ source/gpio.c
@@ -6,6 +6,7 @@
  */
 
 #include "include/gpio.h"
+#include <stdint.h>
 
 void gpio_init(port_e port_num, pin_num_e pin_num, dir_e direction)
 {
@@ -16,11 +17,11 @@ void gpio_init(port_e port_num, pin_num_e pin_num, dir_e direction)
         if( direction == INPUT )
         {
             P1DIR &= ~(1 << pin_num);
-            P1REN = 1 << pin_num;
+            P1REN |= 1 << pin_num;
         }
         else
         {
-            P1DIR = 1 << pin_num;
+            P1DIR |= 1 << pin_num;
         }
 
         P1OUT |= 1 << pin_num;
@@ -32,11 +33,11 @@ void gpio_init(port_e port_num, pin_num_e pin_num, dir_e direction)
         if( direction == INPUT )
         {
             P2DIR &= ~(1 << pin_num);
-            P2REN = 1 << pin_num;
+            P2REN |= 1 << pin_num;
         }
         else
         {
-            P2DIR = 1 << pin_num;
+            P2DIR |= 1 << pin_num;
         }
 
         P2OUT |= 1 << pin_num;
@@ -46,17 +47,17 @@ void gpio_init(port_e port_num, pin_num_e pin_num, dir_e direction)
 bool gpio_get(port_e port_num, pin_num_e pin_num)
 {
     bool val;
-    int pin_val;
+    uint8_t pin_mask;
 
-    pin_val = 1 << pin_num;
+    pin_mask = 1 << pin_num;
 
     if (port_num == PORT_1)
     {
-        val = P1IN & pin_val;
+        val = P1IN & pin_mask;
     }
     else
     {
-        val = P2IN & pin_val;
+        val = P2IN & pin_mask;
     }
 
     return val;
@@ -64,18 +65,29 @@ bool gpio_get(port_e port_num, pin_num_e pin_num)
 
 void gpio_set(port_e port_num, pin_num_e pin_num, bool val)
 {
-    int pin_val;
-    pin_val = 1 << pin_num;
+    uint8_t pin_mask;
+    pin_mask = 1 << pin_num;
 
-    if(val == 0)
+    if(val == 1)
     {
         if (port_num == PORT_1)
         {
-            P1OUT ^= pin_val;
+            P1OUT |= pin_mask;
         }
         else
         {
-            P2OUT ^= pin_val;
+            P2OUT |= pin_mask;
+        }
+    }
+    else
+    {
+        if (port_num == PORT_1)
+        {
+            P1OUT &= ~(pin_mask);
+        }
+        else
+        {
+            P2OUT &= ~(pin_mask);
         }
     }
 }
