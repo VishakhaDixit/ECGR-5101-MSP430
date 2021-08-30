b1ac7c3c1acb0091fb43b883222b954771a33f09
VishakhaDixit <vdixit2@uncc.edu>
2021-08-30 09:35:00 -0400

    Creating separate project folders for main.c files.

---
+++ Project-2/main.c
@@ -0,0 +1,24 @@
+/**
+ * main.c
+ */
+
+#include <include/gpio.h>
+
+int main(void)
+{
+    unsigned int Led, Switch;
+    bool switch_status;
+
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+
+	gpio_init(PORT_2, PIN_1, OUTPUT);   //Using P1.6 for led
+	gpio_init(PORT_1, PIN_3, INPUT);    //Using P1.3 for switch
+	
+	while(1)
+	{
+	    switch_status = gpio_get(PORT_1, PIN_3);
+	    gpio_set(PORT_2, PIN_1, switch_status);
+	}
+
+	return 0;
+}
