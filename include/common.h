e50bc5053015ec26bc751f654fd4a6063df409e8
VishakhaDixit <vdixit2@uncc.edu>
2021-09-20 12:24:59 -0400

    Merged all the pwm and uart files that were given for this project, also most importantly common.h has been replaced with the given file.

--- include/common.h
+++ include/common.h
@@ -1,31 +1,100 @@
-/**************************
+/*
+ * common.h
  *
- * @file    common.h
+ *  Created on: Sep 26, 2018
+ *      Author: Shantanu
+ */
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#include "include/pin_common.h"
+#include "include/DIN.h"
+#include "include/DOUT.h"
+
+/*
+ * Function: pinClearSEL
+ * ----------------------------
+ *   Clears the pin's SEL register.
  *
- * @brief   Common Type Definition for all ports and peripherals.
+ *   port: port number of the pin.
+ *   pin: pin number whose SEL register is to be cleared.
  *
- * @date    Aug 29, 2021
+ *   returns: clears SEL register, returns nothing.
+ */
+void pinClearSEL(unsigned int port, int pin);
+
+/*
+ * Function: pinSetSEL
+ * ----------------------------
+ *   Sets the pin's SEL register.
  *
- * @author  Vishakha Dixit
+ *   port: port number of the pin.
+ *   pin: pin number whose SEL register is to be set.
  *
- **************************/
+ *   returns: sets SEL register, returns nothing.
+ */
+void pinSetSEL(unsigned int port, int pin);
 
-#ifndef COMMON_H_INCLUDE
-#define COMMON_H_INCLUDE
+/*
+ * Function: pinClearSEL2
+ * ----------------------------
+ *   Clears the pin's SEL2 register.
+ *
+ *   port: port number of the pin.
+ *   pin: pin number whose SEL2 register is to be cleared.
+ *
+ *   returns: clears SEL2 register, returns nothing.
+ */
+void pinClearSEL2(unsigned int port, int pin);
 
-#include <msp430.h>
-#include <stdint.h>
-#include <stdbool.h>
+/*
+ * Function: pinSetSEL2
+ * ----------------------------
+ *   Sets the pin's SEL2 register.
+ *
+ *   port: port number of the pin.
+ *   pin: pin number whose SEL2 register is to be set.
+ *
+ *   returns: sets SEL2 register, returns nothing.
+ */
+void pinSetSEL2(unsigned int port, int pin);
 
-typedef enum port
-{
-    PORT_1 = 0,
-    PORT_2 = 1
-} port_e;
+/*
+ * Function: pinSetREN
+ * ----------------------------
+ *   Sets the pin's REN register.
+ *
+ *   port: port number of the pin.
+ *   pin: pin number whose REN register is to be set.
+ *
+ *   returns: sets REN register, returns nothing.
+ */
+void pinSetREN(unsigned int port, int pin);
 
-typedef enum pinNum
-{
-    PIN_0, PIN_1, PIN_2, PIN_3, PIN_4, PIN_5, PIN_6, PIN_7
-} pin_num_e;
+/*
+ * Function: pinClearREN
+ * ----------------------------
+ *   Clears the pin's REN register.
+ *
+ *   port: port number of the pin.
+ *   pin: pin number whose REN register is to be cleared.
+ *
+ *   returns: clears REN register, returns nothing.
+ */
+void pinClearREN(unsigned int port, int pin);
 
-#endif /* COMMON_H_INCLUDE */
+/*
+ * Function: pinSetPull
+ * ----------------------------
+ *   Set's the pin as a pullup or pulldown.
+ *
+ *   port: port number of the pin.
+ *   pin: pin number whose pin is to be set as pullup or pulldown.
+ *   mode: pullup or pulldown mode.
+ *
+ *   returns: set's pin pull based on the mode passed, returns nothing.
+ */
+void pinSetPull(unsigned int port, int pin, int mode);
+
+#endif /* COMMON_H_ */
